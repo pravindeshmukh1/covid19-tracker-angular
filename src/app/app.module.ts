e250987976e18fa3a3c40c8f70646b4f258f175f
pravindeshmukh1 <pravin1desh@gmail.com>
2020-07-26 17:09:55 +0530

    REFACTOR: used scss instead of css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,21 +9,26 @@ import { SidenavComponent } from './component/sidenav/sidenav.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { TotalcountComponent } from './component/totalcount/totalcount.component';
+import { StatecountComponent } from './component/statecount/statecount.component';
+import { SearchboxComponent } from './component/searchbox/searchbox.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
     SidenavComponent,
-    TotalcountComponent
+    TotalcountComponent,
+    StatecountComponent,
+    SearchboxComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
