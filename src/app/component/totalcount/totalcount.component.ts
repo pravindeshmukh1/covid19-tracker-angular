f03b8879943a65cf0d1328384c8e9f5d5aa13ec8
pravindeshmukh1 <pravin1desh@gmail.com>
2020-07-26 17:16:57 +0530

    ADDED: used environment variable for url

--- src/app/component/totalcount/totalcount.component.ts
+++ src/app/component/totalcount/totalcount.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CovidcountService } from 'src/app/services/covidcount.service/covidcount.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-totalcount',
@@ -6,7 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./totalcount.component.scss'],
 })
 export class TotalcountComponent implements OnInit {
-  constructor() {}
+  constructor(
+    private countService: CovidcountService,
+    private snackBar: MatSnackBar
+  ) {}
 
   ngOnInit(): void {}
+
+  getStateData() {
+    this.countService.getStateCount().subscribe(
+      (data) => {
+        console.log('covid19',data);
+      },
+      (err) => {
+        this.snackBar.open('Error Occured at get state count data', ' ', {
+          duration: 2000,
+        });
+      }
+    );
+  }
 }
